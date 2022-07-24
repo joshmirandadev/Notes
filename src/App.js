0fa1bdd8d716af452137e2e0b71662d0895cdf4b
Joshua <joshmatimiranda@gmail.com>
2022-07-24 09:53:50 -0500

    completed new note compononent and delete compononent functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useState } from "react";
 import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
+import Search from "./components/Search";
+import Header from "./components/Header";
 
 const App = () => {
   const [notes, setNotes] = useState([
@@ -22,9 +24,40 @@ const App = () => {
     },
   ]);
 
+  const [searchText, setSearchText] = useState("");
+
+  const [darkMode, setDarkMode] = useState(false);
+
+  const AddNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString(),
+    };
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
   return (
-    <div className="container">
-      <NotesList notes={notes} />
+    <div className={`${darkMode && "dark-mode"}`}>
+      {" "}
+      <div className="container">
+        <Header handleToggleDarkMode={setDarkMode} />
+        <Search handleSearchNote={setSearchText} />
+        <NotesList
+          notes={notes.filter((note) =>
+            note.text.toLowerCase().includes(searchText)
+          )}
+          handleAddNote={AddNote}
+          handleDeleteNote={deleteNote}
+        />
+      </div>
     </div>
   );
 };
